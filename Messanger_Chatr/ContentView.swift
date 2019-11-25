6720150726d604ec9c4ccc2331f0bb1c73975343
Brandon Shaw <unobrandon@mac.com>
2019-11-24 22:42:19 -0500

    Initial Commit

---
+++ Messanger_Chatr/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Messanger_Chatr
+//
+//  Created by Brandon Shaw on 11/24/19.
+//  Copyright © 2019 Brandon Shaw. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
